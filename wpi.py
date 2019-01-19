ca088831f6edb95c5232c9c46e291345598b8254
cyberkyle <43155598+cyberkyle@users.noreply.github.com>
2019-01-18 20:36:15 -0500

    UPDATED 836PM 1/18
    UPDATED 836PM 1/18

--- wpi.py
+++ wpi.py
@@ -3,19 +3,40 @@ from tkinter import *
 import string
 import random
 
+
+def code():
+    #Top Level
+
+    #ENTER TWILLO SEND NUMBER CODE HERE
+    pineapple=(phone_number.get())
+    apple=("+1%s" %pineapple)
+    print(apple)
+    top=Toplevel()
+    top.title=("Code Generator")
+    
+    labela=Label(top, text="Here is your 5 digit code:", font=("Courier New",16))
+    labela.pack()
+
+    random_code=''.join(random.choice(string.digits) for i in range(5))
+    print(random_code)
+
+    labelb=Label(top, text="%s" %random_code, font=("Courier New",30))
+    labelb.pack()
+
+####################___MAIN_LEVEL___#######################################
 root=Tk()
 
-root.title("Security Code Generator")
+root.title("Security Clearence")
 
-labela=Label(root, text="Here is your 5 digit code:", font=("Courier New",16))
-labela.pack()
+label1=Label(root, text="Please enter your phone number below.\nNo - between numbers please.", font=("Courier New",13))
+label1.pack()
 
-random_code=''.join(random.choice(string.digits) for i in range(5))
-print(random_code)
+phone_number=Entry(root)
+phone_number.pack()
 
-labelb=Label(root, text="%s" %random_code, font=("Courier New",30))
-labelb.pack()
-
+button1=Button(root, text="Enter", command=code)
+button1.pack()
 
 root.geometry("400x200")
+
 root.mainloop()
